cdd03c6365e90a76e6b81f992dfb3021bcfacbfd
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:12:40 +0530

    Checking if the Password is Correct:
    const validPassword = await bcryptjs.compare(password, user.password): Compares the provided password with the stored hashed password using bcryptjs.
    If the password is incorrect, it returns a JSON response indicating an invalid password and sets the status code to 400 (Bad Request).
    Logging the User:
    
    console.log(user);: Logs the user object to the console for debugging purposes.

--- app/api/users/login/route.js
+++ app/api/users/login/route.js
@@ -20,11 +20,15 @@ export async function POST(request){
             console.log("User does not exist")
             return NextResponse.json({error: "User does not exist"}, {status: 400})
         }
-        
         console.log("user exists");
         
         
-        
+        //check if password is correct
+        const validPassword = await bcryptjs.compare(password, user.password)
+        if(!validPassword){
+            return NextResponse.json({error: "Invalid password"}, {status: 400})
+        }
+        console.log(user);
         
         
         
