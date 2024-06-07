78357578b9a1d7cb0b4146f44724f49b37678c7d
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 06:52:51 +0530

    Updating Password Reset Tokens:
    If emailType is "RESET", the user's forgotPasswordToken and forgotPasswordTokenExpiry fields are updated.
    forgotPasswordTokenExpiry is set to one hour from the current time.

--- app/(helpers)/mailer.js
+++ app/(helpers)/mailer.js
@@ -12,7 +12,8 @@ export const sendEmail = async({email, emailType, userId}) => {
             await User.findByIdAndUpdate(userId, 
                 {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000});
         } else if (emailType === "RESET") {
-            
+            await User.findByIdAndUpdate(userId, 
+                {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000});
         }
 
         
