5a4adbbc72ce791abb3ad613afc03662c2fa254e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:21:36 +0530

    Importing Dependencies getDataFromToken: A helper function imported from the specified path, used to extract and verify data from a JWT stored in a cookie. NextResponse: A utility from Next.js used to create HTTP responses. User: The User model, imported from the specified path, is used to interact with the users collection in the database. connect: A function imported from the specified path, used to establish a connection to the database.

---
+++ app/api/users/me/route.js
@@ -0,0 +1,8 @@
+import { getDataFromToken } from "@/app/(helpers)/getDataFromToken";
+
+import {NextResponse } from "next/server";
+import User from "@/app/(models)/userModel";
+import { connect } from "@/app/(dbConfig)/dbConfig";
+
+
+
