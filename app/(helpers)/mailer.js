acef412389e689325025961a88beba05da3c59f0
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 06:51:09 +0530

    Creating a Hashed Token Generating the Hashed Token:
    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
    The userId is converted to a string and hashed using bcryptjs with a salt rounds value of 10. This creates a secure, hashed token.
    Encoding the Token:
    
    const encodedToken = encodeURIComponent(hashedToken);
    The hashed token is URL-encoded to ensure it can be safely included in the URL without breaking.

--- app/(helpers)/mailer.js
+++ app/(helpers)/mailer.js
@@ -4,7 +4,9 @@ import bcryptjs from 'bcryptjs';
 
 export const sendEmail = async({email, emailType, userId}) => {
     try {
-        
+        // Create a hashed token
+        const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+        const encodedToken = encodeURIComponent(hashedToken);
 
         
 
