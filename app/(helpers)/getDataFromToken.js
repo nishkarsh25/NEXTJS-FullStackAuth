913ea4c40e08a4cfd75f9f7fe916e0e71719232e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:19:54 +0530

    Extracting the Token:
    const token = request.cookies.get("token")?.value || '';
    This line attempts to retrieve the "token" cookie from the incoming request.
    request.cookies.get("token") gets the cookie object if it exists.
    ?.value safely accesses the value property of the cookie object, even if the cookie object is undefined (using optional chaining).
    || '' ensures that token is an empty string if the cookie is not found, providing a default value.

--- app/(helpers)/getDataFromToken.js
+++ app/(helpers)/getDataFromToken.js
@@ -1,6 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request) => {
-    
+    try {
+        const token = request.cookies.get("token")?.value || '';
+        
+        
+    } catch (error) {
+        
+    }
 
 }
