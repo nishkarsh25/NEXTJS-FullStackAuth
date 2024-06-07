01c9cf23dad86a3161965b1749e6766c8f81ee91
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:02:22 +0530

    Hashing the Password:
    const salt = await bcryptjs.genSalt(10): Generates a salt with 10 rounds, which adds an extra layer of security to the password hashing process.
    const hashedPassword = await bcryptjs.hash(password, salt): Hashes the password using the generated salt.

--- app/api/users/signup/route.js
+++ app/api/users/signup/route.js
@@ -24,7 +24,9 @@ export async function POST(request) {
             return NextResponse.json({ error: "User already exists" }, { status: 400 })
         }
 
-        
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
 
         
 
