ba2ed715298310632e2aab428b949b1998f1c2ce
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:37:36 +0530

    State Variables:
    data: A state variable initialized to "nothing" to store user details or a default value.

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -7,7 +7,8 @@ import {useRouter} from "next/navigation";
 
 
 export default function ProfilePage() {
-    
+    const router = useRouter()
+    const [data, setData] = useState("nothing")
     
 
     
