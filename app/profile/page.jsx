116fb8605dd989059b5021f58f1efea8ca24e241
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:37:08 +0530

    Let's break down the provided ProfilePage component step by step to understand its functionality and purpose in detail.
    Importing Dependencies
    "use client";: Indicates that this code is meant to run on the client side in a Next.js application.
    axios: A library used to make HTTP requests.
    Link from "next/link": Used to create client-side navigable links.
    React: Imported to use React features such as hooks and components.
    useState: A React hook to manage state in the component.
    toast from "react-hot-toast": Used for displaying notifications.
    useRouter from "next/navigation": Provides navigation functionalities.

---
+++ app/profile/page.jsx
@@ -0,0 +1,7 @@
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React, {useState} from "react";
+import {toast} from "react-hot-toast";
+import {useRouter} from "next/navigation";
+
