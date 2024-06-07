cf0c3348d3ba345b93ad5a466f1896c795a617d5
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:33:17 +0530

    Importing Dependencies "use client";: Indicates that this code is meant to run on the client side in a Next.js application. Link from "next/link": Used to create client-side navigable links. React: Imported to use React features such as hooks and components. useRouter from "next/navigation": Provides navigation functionalities. axios: A library used to make HTTP requests. toast from "react-hot-toast": Used for displaying notifications.

---
+++ app/login/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+
+
+
+
