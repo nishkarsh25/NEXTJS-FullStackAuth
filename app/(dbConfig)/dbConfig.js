35bb8f0c3f778da352310f6ddc184be93fb71f8a
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 06:45:30 +0530

    Check Connection Status:
    The function first checks if isConnected is true. If it is, this means a connection to the database already exists.
    If a connection already exists, it logs a message ('Using existing database connection') and returns immediately, avoiding the need to establish a new connection.

--- app/(dbConfig)/dbConfig.js
+++ app/(dbConfig)/dbConfig.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 let isConnected = false; // Global variable to track connection status
 
 export async function connect() {
-    
+    if (isConnected) {
+        console.log('Using existing database connection');
+        return;
+    }
 
     
 }
