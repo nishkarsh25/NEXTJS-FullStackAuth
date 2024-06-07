f0295b44c8916f4c7a25f9c43588fdbc6c3ece7c
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:22:27 +0530

    Extracting the User ID from the Token:
    const userId = await getDataFromToken(request);: This function extracts the user ID from the JWT stored in the request's cookies using the getDataFromToken helper function.

--- app/api/users/me/route.js
+++ app/api/users/me/route.js
@@ -11,7 +11,7 @@ export async function GET(request){
     try {
 
         await connect();
-        
+        const userId = await getDataFromToken(request);
         
         
     } catch (error) {
