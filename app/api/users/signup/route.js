2ad28104bf847870293ed021c31e77083e59132b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:00:43 +0530

    Importing Dependencies connect: Imported from the dbConfig file, this function is used to establish a connection to the database. User: The User model, imported from the userModel file, is used to interact with the users collection in the database. NextResponse: A utility from Next.js used to create HTTP responses. bcryptjs: A library used for hashing passwords. sendEmail: A helper function imported from the mailer file, used to send verification emails.

---
+++ app/api/users/signup/route.js
@@ -0,0 +1,10 @@
+import { connect } from "@/app/(dbConfig)/dbConfig";
+import User from "@/app/(models)/userModel";
+import {NextResponse} from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/app/(helpers)/mailer";
+
+
+
+
+
