91d63137dca452bb0b0aec6c9c7a8e0bf00f6b88
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:34:02 +0530

    State Variables:
    user: An object containing the email and password entered by the user.
    buttonDisabled: A boolean indicating whether the login button should be disabled.
    loading: A boolean indicating whether a login request is currently being processed.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -10,8 +10,14 @@ import { toast } from "react-hot-toast";
 
 
 export default function LoginPage() {
-    
-    
+    const router = useRouter();
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
 
     
