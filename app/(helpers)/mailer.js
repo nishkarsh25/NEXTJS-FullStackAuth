c7135dde8fed5ba73ab9fb22990c81a1a4e0c65b
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 06:50:19 +0530

    sendEmail Function This function handles sending verification or password reset emails to users.
    Parameters:
    email: The recipient's email address.
    emailType: A string indicating the type of email to send. It can be either "VERIFY" or "RESET".
    userId: The ID of the user for whom the email is being sent.

--- app/(helpers)/mailer.js
+++ app/(helpers)/mailer.js
@@ -2,4 +2,6 @@ import nodemailer from 'nodemailer';
 import User from "@/app/(models)/userModel";
 import bcryptjs from 'bcryptjs';
 
-
+export const sendEmail = async({email, emailType, userId}) => {
+    
+};
