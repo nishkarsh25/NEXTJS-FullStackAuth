6492cd4b66f5f3cc5e6e9a56332a997ab238194e
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:34:53 +0530

    onLogin Function:
    Purpose: Sends the user's email and password to the backend for authentication.
    Steps:
    Sets loading to true to indicate the start of the login process.
    Sends a POST request to /api/users/login with the user's email and password using axios.
    If the request is successful:
    Logs a success message and displays a success toast notification.
    Redirects the user to the /profile page using router.push.
    If there's an error:
    Logs the error message and displays an error toast notification.
    Sets loading to false to indicate the end of the login process.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -21,7 +21,18 @@ export default function LoginPage() {
 
 
     const onLogin = async () => {
-        
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
+            router.push("/profile");
+        } catch (error) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
     
