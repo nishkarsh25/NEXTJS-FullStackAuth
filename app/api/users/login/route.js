5f00bdbd04f14bce6e793338ca76158b19efe126
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:12:06 +0530

    Checking if the User Exists:
    const user = await User.findOne({ email }): Searches for a user with the given email in the database.
    If no user is found, it returns a JSON response indicating that the user does not exist and sets the status code to 400 (Bad Request).

--- app/api/users/login/route.js
+++ app/api/users/login/route.js
@@ -14,7 +14,13 @@ export async function POST(request){
         const {email, password} = reqBody;
         console.log(reqBody);
 
-        
+        //check if user exists
+        const user = await User.findOne({email})
+        if(!user){
+            console.log("User does not exist")
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+        console.log("user exists");
         
         
         
