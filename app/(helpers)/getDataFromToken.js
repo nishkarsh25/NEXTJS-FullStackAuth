87a9a86b460e04e39c95bd09642fcc105ebbaef7
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:20:19 +0530

    Verifying the Token:
    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
    jwt.verify(token, process.env.TOKEN_SECRET) verifies the JWT using the secret key stored in process.env.TOKEN_SECRET.
    If the token is valid, jwt.verify decodes the token and returns the decoded payload (i.e., the data stored in the token).

--- app/(helpers)/getDataFromToken.js
+++ app/(helpers)/getDataFromToken.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const getDataFromToken = (request) => {
     try {
         const token = request.cookies.get("token")?.value || '';
-        
+        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
         
     } catch (error) {
         
