30418fcd7263f9f9c6991577bf910f23bf074c01
nishkarsh25 <bm21btech11016@iith.ac.in>
2024-06-07 07:13:58 +0530

    Creating the Response:
    const response = NextResponse.json({ message: "Login successful", success: true, }): Creates a JSON response indicating a successful login.
    
    Setting the Cookie:
    
    response.cookies.set("token", token, { httpOnly: true }): Sets a cookie named "token" with the JWT, making it HTTP-only for security purposes.

--- app/api/users/login/route.js
+++ app/api/users/login/route.js
@@ -39,7 +39,14 @@ export async function POST(request){
         //create token
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, {expiresIn: "1d"})
 
-        
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+        response.cookies.set("token", token, {
+            httpOnly: true, 
+            
+        })
         
 
     } catch (error) {
